f417a23f9904b31df30b8906f0047b000b6425cd
nealzhu <nealzhuqian@163.com>
2015-04-02 23:54:48 +0800

    头部尾部完成

--- js/main.js
+++ js/main.js
@@ -12,20 +12,39 @@ $(document).ready(function() {
         subMenu.height(itemHeight);
         nav.addClass('nav-on');
         treeItem.addClass('on').siblings().removeClass('on');
-        panel.stop().animate({
-            height : itemHeight + 140
-        }, 300);
+        panel.height(itemHeight + 140);
     });
 
     panel.on('mouseleave', function(evt) {
         var relatedTarget = $(evt.relatedTarget);
         if(!relatedTarget.hasClass('has-sub-tree') && !relatedTarget.parent().hasClass('has-sub-tree')) {
             nav.removeClass('nav-on');
-            panel.stop().animate({
-                height : 0
-            }, 300);
+            panel.height(0);
         }
         subMenu && subMenu.height(0);
         treeItem.removeClass('on'); 
+    });
+
+    var searchBtn = $('#search-btn');
+    var searchClose = $('#search-close');
+    var searchPanel = searchBtn.next();
+    var onSearch = false;
+
+    searchBtn.on('click', function(evt) {
+        evt.stopPropagation();
+        onSearch = true;
+        searchPanel.height(80);
+        searchPanel.find('input').focus();
+    });
+
+    searchClose.on('click', function() {
+        onSearch = false;
+        searchPanel.height(0);
+    });
+
+    $(document).on('click', function(evt) {
+        if(onSearch && !searchPanel[0].contains(evt.target)) {
+            searchPanel.height(0);
+        }
     });
 });
