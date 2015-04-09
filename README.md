0a0da841049bf06c4ff83bc62831556b7a1f26b9
nealzhu <nealzhuqian@163.com>
2015-04-09 23:57:26 +0800

    首页和新闻页完成

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # Rowe office site
+
+## Bugs
+
+- ~~IE8 下导航特效有问题~~
+- 顶部大图自适应不完美
--- js/main.js
+++ js/main.js
@@ -1,9 +1,10 @@
+// 导航特效
 $(document).ready(function() {
     var nav = $('#nav');
     var panel = $('#rw-nav-panel');
     var subMenu, treeItem;
 
-    nav.on('mouseenter', '.has-sub-tree', function() {
+    nav.on('mouseover', '.has-sub-tree', function() {
         var subMenuItem, itemHeight;
         treeItem = $(this);
         subMenu = treeItem.find('.rw-sub-nav');
@@ -15,7 +16,7 @@ $(document).ready(function() {
         panel.height(itemHeight + 140);
     });
 
-    panel.on('mouseleave', function(evt) {
+    panel.on('mouseout', function(evt) {
         var relatedTarget = $(evt.relatedTarget);
         if(!relatedTarget.hasClass('has-sub-tree') && !relatedTarget.parent().hasClass('has-sub-tree')) {
             nav.removeClass('nav-on');
@@ -48,3 +49,41 @@ $(document).ready(function() {
         }
     });
 });
+
+// 首页
+$(document).ready(function() {
+    var figures = $('#rw-header-bg-wrap');
+    var btns = $('.rw-header-btn');
+    var sliderCtrl = $('#rw-slider');
+    var thumbs = sliderCtrl.find('.rw-slider-thumb');
+    var current = 0;
+
+    $('#rw-slider').on('click', '.rw-slider-thumb', function() {
+        current = $(this).data('index');
+        $(this).addClass('on').siblings().removeClass('on');
+        btns.hide().eq(current).show();
+        figures.stop().animate({
+            left : -current * 1 + '00%'
+        }, 500);
+    }).on('click', '.rw-slider-prev', function() {
+        current = (current === 0) ? 2 : (current - 1);
+        thumbs.removeClass('on').eq(current).addClass('on');
+        btns.hide().eq(current).show();
+        figures.stop().animate({
+            left : -current * 1 + '00%'
+        }, 500);
+    }).on('click', '.rw-slider-next', function() {
+        current = (current + 1) % 3;
+        thumbs.removeClass('on').eq(current).addClass('on');
+        btns.hide().eq(current).show();
+        figures.stop().animate({
+            left : -current * 1 + '00%'
+        }, 500);
+    });
+});
+
+$(document).ready(function() {});
+$(document).ready(function() {});
+$(document).ready(function() {});
+$(document).ready(function() {});
+$(document).ready(function() {});
